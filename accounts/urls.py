3eb239e30785411f6ee09afbe135004a89125024
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2019-10-03 22:30:02 +0700

    creting login template

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
-    path('', views.home)
+    path('', views.home),
+    path('login/', LoginView.as_view(template_name='accounts/login.html'))
 ]
